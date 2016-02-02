f2d0a0c417ae6c240935e5c340d4a6b7dbd995e5
Pascal Held <pascal-held@t-online.de>
2016-02-02 22:01:37 +0100

    Fix bug causing file system access violation.

--- bots/binarydetector/run.sh
+++ bots/binarydetector/run.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-cd $1
-find . -type f | xargs -I{} bash -c "file {}" | grep -v "ASCII text" | grep -v "empty" > result.txt
+CWD=$(pwd)
+RESULT_FILE=${CWD}/result.txt
 
-LINES=$(cat result.txt | wc -l)
+cd $1
+find . -type f | xargs -I{} bash -c "file {}" | grep -v "ASCII text" | grep -v "empty" > ${RESULT_FILE}
+
+LINES=$(cat ${RESULT_FILE} | wc -l)
 
 if [[ ${LINES} != 0 ]]
 then
-        cat result.txt
+        cat ${RESULT_FILE}
         exit 1
 else
         exit 0
