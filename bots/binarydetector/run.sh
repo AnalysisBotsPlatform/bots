6540d12c7aa7ff45f7a0978de3e2393f66e8b13f
Pascal Held <pascal-held@t-online.de>
2016-02-02 22:19:09 +0100

    Fix bug causing false positives due to traversing the .git directory.

--- bots/binarydetector/run.sh
+++ bots/binarydetector/run.sh
@@ -4,7 +4,7 @@ CWD=$(pwd)
 RESULT_FILE=${CWD}/result.txt
 
 cd $1
-find . -type f | xargs -I{} bash -c "file {}" | grep -v "ASCII text" | grep -v "empty" > ${RESULT_FILE}
+find . -type f ! -path './.git/*' | xargs -I{} bash -c "file {}" | grep -v "ASCII text" | grep -v "empty" > ${RESULT_FILE}
 
 LINES=$(cat ${RESULT_FILE} | wc -l)
 
