3235b425966f8c7fcafeeaa7f8ba2a04891332a9
Pascal Held <pascal-held@t-online.de>
2016-02-05 17:48:03 +0100

    Implement git patch feature.

--- bots/binarydetector/run.sh
+++ bots/binarydetector/run.sh
@@ -1,17 +1,27 @@
 #! /bin/bash
 
 CWD=$(pwd)
-RESULT_FILE=${CWD}/result.txt
+RESULT_FILE="${CWD}"/result.txt
 
-cd $1
-find . -type f ! -path './.git/*' | xargs -I{} bash -c "file {}" | grep -v "ASCII text" | grep -v "empty" > ${RESULT_FILE}
+cd "$1"
+find . -type f ! -path './.git/*' | xargs -I{} bash -c "file {}" | grep -v "ASCII text" | grep -v "empty" > "${RESULT_FILE}"
 
-LINES=$(cat ${RESULT_FILE} | wc -l)
+LINES=$(cat "${RESULT_FILE}" | wc -l)
 
 if [[ ${LINES} != 0 ]]
 then
-        cat ${RESULT_FILE}
-        exit 1
-else
-        exit 0
+        cat "${RESULT_FILE}"
+        cd "${CWD}"
+        cp -R "$1/." "$2/"
+        cd "$2"
+        while read LINE
+        do
+                echo ${LINE} | awk '{ split($0, a, ":"); print a[1] }' | xargs -I{} bash -c "git rm '{}' > /dev/null"
+        done < "${RESULT_FILE}"
+        git config user.name "BinaryDetectorBot"
+        git config user.email "noreply@analysisbots.com"
+        git commit -m "Remove binary files automatically." > /dev/null
+        git format-patch @^ --stdout > changes.patch
 fi
+
+exit 0
